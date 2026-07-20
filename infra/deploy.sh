c61a364e2f3e38d99be45e19403e782357a3b6a9
Ivon Matos <ivon.asmatos@senacsp.edu.br>
2026-07-20 12:56:14 -0300

    garante permissões e ambiente no deploy

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -eu
 cd /opt/dashbv
+set -a
+. ./.env
+set +a
+mkdir -p data server-data/doc
+chown -R 10001:10001 server-data
 docker compose -f compose.yaml -f compose.server.yaml pull db
 docker compose -f compose.yaml -f compose.server.yaml build --pull api worker
 docker compose -f compose.yaml -f compose.server.yaml up -d db api worker
